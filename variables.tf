9507ae56341f8665d80457bd3d955b0dc2152a19
dhulkii <dhulkii.dhulkifilkanhirot@gmail.com>
2024-07-23 09:01:07 +0000

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+  description = "ami value"
+  default     = "ami-0195204d5dce06d99"
+}
+variable "instance_type" {
+  description = "value for instance type"
+  default     = "t2.micro"
+}
+variable "key_name" {
+  description = "value fro the key pair"
+  default     = "dk"
+}
